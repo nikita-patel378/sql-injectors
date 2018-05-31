d42c8f569e49ac885922a96d2511bb2ec3f75aff
Nikita <nix1228@gmail.com>
2018-05-30 20:51:48 -0700

    the final cleanup

--- app.py
+++ app.py
@@ -16,64 +16,63 @@ app = Flask(__name__)
 #################################################
 from flask_sqlalchemy import SQLAlchemy
 # The database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data/raw_data/honeyproduction.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data/clean_data/honeyproduction.sqlite"
 
 db = SQLAlchemy(app)
 
 
 class HoneyProd(db.Model):
-    __tablename__ = 'honeyproduction'
+    __tablename__ = 'honey_pest_1991_2017'
+    id = db.Column(db.Integer, primary_key = True)
+    state = db.Column(db.String)
+    year = db.Column(db.Integer)
+    state_name = db.Column(db.String)
+    numcol = db.Column(db.Integer)
+    yieldpercol = db.Column(db.Numeric)
+    totalprod = db.Column(db.Numeric)
+    stocks = db.Column(db.Numeric)
+    priceperlb = db.Column(db.Numeric)
+    prodvalue = db.Column(db.Numeric)
+    totalpest = db.Column(db.Numeric)
+
+    __tablename__ = 'honeyproduction_withlatlon'
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String)
-    numcol = db.Column(db.String)
-    yieldpercol = db.Column(db.String)
-    totalprod = db.Column(db.String)
-    stocks = db.Column(db.String)
-    priceperlb = db.Column(db.String)
-    prodvalue = db.Column(db.String)
-    year = db.Column(db.String)
+    state_full=db.Column(db.String)
+    numcol = db.Column(db.Integer)
+    yieldpercol = db.Column(db.Numeric)
+    totalprod = db.Column(db.Numeric)
+    stocks = db.Column(db.Numeric)
+    priceperlb = db.Column(db.Numeric)
+    prodvalue = db.Column(db.Numeric)
+    year = db.Column(db.Integer)
+    latitude = db.Column(db.Numeric)
+    longitude = db.Column(db.Numeric)
+    state_yr = db.Column(db.String)
+    temp_min = db.Column(db.Numeric)
+    temp_max = db.Column(db.Numeric)
+    temp_minC = db.Column(db.Numeric)
+    temp_maxC = db.Column(db.Numeric)
+
+    
+
 
     def __repr__(self):
         return '<HoneyProd %r>' % (self.name)
 
 
-# now for the leaflet map
-from flask_sqlalchemy import SQLAlchemy
-# The database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data/raw_data/honeyproduction_withlatlon.sqlite"
-
-db = SQLAlchemy(app)
-
-
-class HoneyProdCoord(db.Model):
-    __tablename__ = 'honeyproduction_withlatlon'
-    id = db.Column(db.Integer, primary_key=True)
-    state = db.Column(db.String)
-    numcol = db.Column(db.String)
-    yieldpercol = db.Column(db.String)
-    totalprod = db.Column(db.String)
-    stocks = db.Column(db.String)
-    priceperlb = db.Column(db.String)
-    prodvalue = db.Column(db.String)
-    year = db.Column(db.String)
-    latitude = db.Column(db.String)
-    longitude = db.Column(db.String)
-    state_yr = db.Column(db.String)
-
-    def __repr__(self):
-        return '<HoneyProdCoord %r>' % (self.name)
 
 
 # Create database tables
 @app.before_first_request
 def setup():
     # Recreate database each time for demo
-    # db.drop_all()
+  #  db.drop_all()
     db.create_all()
 
 ##########################################################
- ##############The Routes#################################
-
+ #                   The Routes
+##########################################################
 
 @app.route("/")
 def main():
@@ -89,21 +88,9 @@ def home():
 def about():
     return render_template("about.html")
 
-
-@app.route("/bubble")
-def bubble():
-    return render_template("bubble.html")
-
-
-@app.route("/map")
-def map():
-    return render_template("map.html")
-
-
 @app.route("/colony")
 def colony():
     return render_template("colony.html")
-
 
 @app.route("/particles")
 def particles():
@@ -115,18 +102,25 @@ def particles():
     return jsonify(particles.to_dict(orient="records"))
 
 
-@app.route("/data")
-def data():
+@app.route("/bubble")
+def bubble():
+    return render_template("bubble.html")
 
-    df = pd.read_sql_table("honeyproduction", "sqlite:///data/raw_data/honeyproduction.sqlite")
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/honey-pest")
+def honey_pest():
+
+    df = pd.read_sql_table("honey_pest_1991_2017", "sqlite:///data/clean_data/honeyproduction.sqlite")
 
     return jsonify(df.to_dict(orient="records"))
-
 
 @app.route("/map-data")
 def map_data():
 
-    df = pd.read_csv("data/raw_data/honeyproduction_withlatlon.csv")
+    df = pd.read_sql_table("honeyproduction_withlatlon", "sqlite:///data/clean_data/honeyproduction.sqlite")
     data = []
     for i, row in df.iterrows():
         data.append({
@@ -147,7 +141,7 @@ def map_data():
 @app.route("/temp-data2")
 def temp_data2():
     path_to_file = 'static/json/us-states.json'
-    temp_df = pd.read_csv("data/raw_data/honeyproduction_withlatlon.csv")
+    temp_df = pd.read_sql_table("honeyproduction_withlatlon", "sqlite:///data/clean_data/honeyproduction.sqlite")
 
     with open(path_to_file) as f:
         json_data = geojson.load(f)
@@ -170,20 +164,7 @@ def temp_data2():
     return jsonify(json_data)
 
 
-@app.route("/honey-pest")
-def honey_pest():
 
-    df = pd.read_csv("data/clean_data/honey_pest_1991_2017.csv", encoding='utf-8')
-
-    return jsonify(df.to_dict(orient="records"))
-
-
-@app.route("/honey-temp-pest")
-def honey_temp():
-
-    df = pd.read_csv("data/clean_data/honey_temp_pest_1991_2013.csv", encoding='utf-8')
-
-    return jsonify(df.to_dict(orient="records"))
 
 
 if __name__ == "__main__":
